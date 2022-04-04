ccc98c2f6e0522843f3bdf5b8403588dfe9a81b9
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-04-05 03:04:55 +0700

    update search, pagination, readme

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ basic movie web
 
 # screenshoot
 
+### Homepage
 ![img.png](assets/img.png)
 ![img_1.png](assets/img_1.png)
+### Movie details
 ![img_2.png](assets/img_2.png)
 ![img_3.png](assets/img_3.png)
+### Movie Search
+![img.png](assets/img_4.png)
--- src/page/MoviePage.jsx
+++ src/page/MoviePage.jsx
@@ -3,29 +3,48 @@ import useSWR from "swr";
 import { apiKey, fetcher } from "../config/config";
 import MovieCard from "../components/movie/MovieCard";
 import useDebounce from "../hooks/useDebounce";
+import ReactPaginate from "react-paginate";
+const itemsPerPage = 20;
 
 const MoviePage = () => {
+  const [pageCount, setPageCount] = React.useState(0);
+  const [itemOffset, setItemOffset] = React.useState(0);
+  const [nextPage, setNextPage] = React.useState(1);
   const [filter, setFilter] = React.useState("");
   const [url, setUrl] = React.useState(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
+    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${nextPage}`
   );
   const filterDebounce = useDebounce(filter, 1000);
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
   };
-
   const { data, error } = useSWR(url, fetcher);
-
+  const loading = !data && !error;
   React.useEffect(() => {
     if (filterDebounce) {
       setUrl(
-        `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${filterDebounce}`
+        `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${filterDebounce}&page=${nextPage}`
       );
     } else {
-      setUrl(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+      setUrl(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${nextPage}`
+      );
     }
-  }, [filterDebounce]);
+  }, [filterDebounce, nextPage]);
+
   const movies = data?.results || [];
+
+  React.useEffect(() => {
+    if (!data || !data.total_results) return;
+    setPageCount(Math.ceil(data.total_results / itemsPerPage));
+  }, [data, itemOffset]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.total_results;
+    setItemOffset(newOffset);
+    setNextPage(event.selected + 1);
+  };
+
   return (
     <div className="py-10 page-container">
       <div className="flex mb-10">
@@ -54,8 +73,12 @@ const MoviePage = () => {
           </svg>
         </button>
       </div>
+      {loading && (
+        <div className="w-10 h-10 rounded-full border-4 border-primary border-t-transparent border-t-4 mx-auto animate-spin" />
+      )}
       <div className="grid grid-cols-4 gap-10">
-        {movies.length > 0 &&
+        {!loading &&
+          movies.length > 0 &&
           movies.map((movie) => (
             <MovieCard
               key={movie.id}
@@ -66,6 +89,19 @@ const MoviePage = () => {
               id={movie.id}
             />
           ))}
+      </div>
+
+      <div className="flex items-center justify-center mt-10 gap-x-5">
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          className="pagination"
+        />
       </div>
     </div>
   );
